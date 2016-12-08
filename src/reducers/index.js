c4296a8a762553b7a37b65ffa6057a5b48089536
itintern <itintern>
2016-12-08 17:23:08 +1100

    ActionCreator for Selected Book detail

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  // Adding key to our global application state called books
+  // Where the key is books and the value is whatever gets returned
+  // from the books reducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
